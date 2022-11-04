14c21160b9da792dc2854eaaa1cc567c54321022
Janio Isacura <janioisacura@gmail.com>
2022-11-04 12:39:40 -0300

    Fetching products from mocked service

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -9,7 +9,7 @@ import { AppRoutingModule } from '../app-routing.module';
 @NgModule({
   declarations: [
     ProductsComponent,
-    ProductComponent
+    ProductComponent,
   ],
   imports: [
     CommonModule,
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -10,8 +10,11 @@ export class ProductsService {
     { id: 1, name: 'Toaster', description: 'A regular toaster' },
     { id: 2, name: 'TV', description: 'A big ass TV' },
     { id: 3, name: 'Electric guitar', description: 'A device used to impress people or annoy neighbours' },
-    { id: 5, name: 'PS5', description: "If you have the money we won't have it" },
-    { id: 6, name: 'Toaster', description: 'A regular toaster' },
+    { id: 4, name: 'PS5', description: "If you have the money we won't have it" },
+    { id: 5, name: 'Crappy office notebook', description: 'Expensive as f*ck, cant run sh*t in it' },
+    { id: 6, name: 'Gaming mouse', description: "It won't improve your aim" },
+    { id: 7, name: 'Gaming keyboard', description: 'Because it has the word gaming in it' },
+    { id: 8, name: 'Gaming waifu', description: 'Instructions not included' },
   ]
 
   constructor() { }
