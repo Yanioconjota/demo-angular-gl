cc08d5790134f444d870744702b25b949af8939a
Janio Isacura <janioisacura@gmail.com>
2022-11-04 15:49:18 -0300

    product detail component added

---
+++ src/app/products/product-detail/product-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit, OnDestroy {
+
+  subscriber!: Subscription;
+  product!: Product;
+  productId!: number;
+
+  constructor(private routeParams: ActivatedRoute,
+              private router: Router,
+              private productsService: ProductsService) { }
+
+  ngOnInit(): void {
+    this.productId = +this.routeParams.snapshot.url[1].path;
+    console.log(this.productId);
+    this.subscriber = this.productsService.getProductById(this.productId)
+        .subscribe(product => this.product = product);
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscriber) {
+      this.subscriber.unsubscribe();
+    }
+  }
+
+}
