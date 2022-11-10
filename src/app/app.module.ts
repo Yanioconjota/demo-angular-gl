618534bac5967643ae155ba429f20814296d542c
Janio Isacura <janioisacura@gmail.com>
2022-11-10 18:28:47 -0300

    NgRx devTools added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { SharedModule } from './shared/shared.module';
 import { ProductsModule } from './products/products.module';
 import { productsReducer } from './products/state/products.reducer';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,11 @@ import { StoreModule } from '@ngrx/store';
     AppRoutingModule,
     StoreModule.forRoot({
       products: productsReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+      autoPause: true
     })
   ],
   providers: [],
